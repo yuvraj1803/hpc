b04b8172055ad52ea19275c96cfc1142a45a8772
yuvraj1803 <ysakshith@gmail.com>
2025-12-31 21:27:06 +0530

    hpc: add tools/timer.h for time measurement helpers

---
+++ tools/timer.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <time.h>
+
+static inline double hpc_now(void) {
+    struct timespec t;
+    clock_gettime(CLOCK_MONOTONIC, &t);
+    return (double)t.tv_sec + (double)t.tv_nsec * 1e-9;
+}
+
