d938f490c6a15a8235bab482092401180802a0b0
yuvraj1803 <ysakshith@gmail.com>
2026-01-01 11:00:50 +0530

    hpc: sum0: separated time accounting from input

--- src/sum/sum0.c
+++ src/sum/sum0.c
@@ -2,13 +2,30 @@
 #include <stdlib.h>
 #include "timer.h"
 
+#define ARR_SIZE	(1024*1024*1024)
+int *arr;
+
 int main() {
     long long sum = 0;
-    int x;
+    arr = (int*) malloc(sizeof(int) * ARR_SIZE);
+    if(!arr) {
+		printf("ENOMEM: arr\n");
+		return 1;
+	}
 
+	int x;
+	int i = 0;
+    while (scanf("%d", &x) == 1) {
+		arr[i++] = x;
+		if(i == ARR_SIZE) break;
+	}
+	int size = i;
     double t0 = hpc_now();
-    while (scanf("%d", &x) == 1)
-        sum += x;
+	
+	for(i = 0; i < size; i++) {
+		sum += arr[i];
+	}
+
     double t1 = hpc_now();
 
     printf("time=%f sum=%lld\n", t1 - t0, sum);
