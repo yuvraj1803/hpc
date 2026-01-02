b17ea6d42336b8888d5cb37c308dc01daee9acb4
yuvraj1803 <ysakshith@gmail.com>
2026-01-02 23:11:32 +0530

    hpc: conv0: added convolution with tiling and ILP4

---
+++ src/convolution/conv0_tiling_ILP4.c
@@ -0,0 +1,102 @@
+#include "timer.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+double time_begin;
+double time_end;
+
+struct tensor_2d {
+	int r;
+	int c;
+	float *data;
+};
+
+struct tensor_2d *grid;
+struct tensor_2d *kernel;
+struct tensor_2d *out;
+
+struct tensor_2d *read_tensor() {
+	struct tensor_2d *t = (struct tensor_2d *) malloc(sizeof(struct tensor_2d));
+    scanf("%d %d", &t->r, &t->c);
+    t->data = malloc(sizeof(float) * t->r * t->c);
+    if (!t->data) {
+        fprintf(stderr, "ENOMEM\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < t->r * t->c; i++)
+        scanf("%f", &t->data[i]);
+
+    return t;
+}
+
+void input() {
+	grid = read_tensor();
+	kernel = read_tensor();
+
+	if(!grid || !kernel) {
+		printf("error: could not read input tensors\n");
+		exit(1);
+	}
+}
+
+void output() {
+
+	printf("grid: %d x %d\n", grid->r, grid->c);
+	printf("kernel: %d x %d\n", kernel->r, kernel->c);
+	printf("output: %d x %d\n", out->r, out->c);
+
+	printf("time: %f\n", (time_end - time_begin));
+
+}
+
+void precompute() {
+	out = malloc(sizeof(struct tensor_2d));
+	out->r = grid->r - kernel->r + 1;
+	out->c = grid->c - kernel->c + 1;
+	out->data = malloc(sizeof(float) * out->r * out->c);
+}
+
+#define TILE_ROWS	8
+#define TILE_COLS	8
+
+
+void compute() {
+
+	for(int i = 0; i < out->r; i += TILE_ROWS) {
+		for(int j = 0; j < out->c; j += TILE_COLS) {
+
+			int i_end = (i + TILE_ROWS < out->r) ? i + TILE_ROWS : out->r;
+			int j_end = (j + TILE_COLS < out->c) ? j + TILE_COLS : out->c;
+
+			for(int ti = i; ti < i_end; ti++) {
+				for(int tj = j; tj < j_end; tj++) {
+					float sum = 0.0;
+					for(int kr = 0; kr < kernel->r; kr++) {
+						for(int kc = 0; kc + 3 < kernel->c; kc+=4) {
+							sum += grid->data[(ti + kr) * grid->c + (tj + kc)] * kernel->data[kr * kernel->c + kc];
+							sum += grid->data[(ti + kr) * grid->c + (tj + kc + 1)] * kernel->data[kr * kernel->c + kc + 1];
+							sum += grid->data[(ti + kr) * grid->c + (tj + kc + 2)] * kernel->data[kr * kernel->c + kc + 2];
+							sum += grid->data[(ti + kr) * grid->c + (tj + kc + 3)] * kernel->data[kr * kernel->c + kc + 3];
+						}
+					}
+					out->data[ti * out->c + tj] = sum;
+				}
+			}
+		}
+	}
+
+}
+
+int main() {
+	
+	input();
+
+	precompute();
+
+	time_begin = hpc_now();
+	compute();
+	time_end = hpc_now();
+
+	output();
+}
